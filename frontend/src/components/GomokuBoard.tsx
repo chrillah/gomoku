8287c4436868aa5f744e73f76046e2662086a1d5
igge <igge88@hotmail.com>
2023-10-31 10:39:07 +0100

    adding logic, routes and some css

--- frontend/src/components/GomokuBoard.tsx
+++ frontend/src/components/GomokuBoard.tsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import GomokuTile from './GomokuTile';
+import React from 'react'
+import GomokuTile from './GomokuTile'
 
 interface BoardProps {
     boardData: {
-        minInRow: number;
-        cols: number;
-        rows: number;
-        tiles: number[][];
-    };
+        minInRow: number
+        cols: number
+        rows: number
+        tiles: number[][]
+    }
+    // MIN VARIANT
+    makeMove: (row: number, col: number) => void
 }
 
-const GomokuBoard: React.FC<BoardProps> = ({ boardData }) => {
-    const { tiles } = boardData;
+const GomokuBoard: React.FC<BoardProps> = ({ boardData, makeMove }) => {
+    const { tiles } = boardData
+
+    // MIN VARIANT
+    const handleTileClick = (row: number, col: number) => {
+        if (tiles[row][col] === 0) {
+            makeMove(row, col)
+        }
+    }
 
     return (
         <div className="gomoku-board">
             {tiles.map((row, rowIndex) => (
                 <div key={rowIndex} className="gomoku-row">
                     {row.map((tile, colIndex) => (
-                        <GomokuTile key={colIndex} player={tile !== 0 ? (tile === 1 ? 'black' : 'white') : null} />
+                        // MIN VARIANT
+                        <div
+                            key={colIndex}
+                            onClick={() => handleTileClick(rowIndex, colIndex)}
+                        >
+                            <GomokuTile
+                                player={
+                                    tile !== 0
+                                        ? tile === 1
+                                            ? 'black'
+                                            : 'white'
+                                        : null
+                                }
+                            />
+                        </div>
                     ))}
                 </div>
             ))}
         </div>
-    );
-};
+    )
+}
 
-export default GomokuBoard;
+export default GomokuBoard
--- frontend/src/components/GomokuGame.tsx
+++ frontend/src/components/GomokuGame.tsx
@@ -1,31 +1,199 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import GomokuBoard from './GomokuBoard';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import GomokuBoard from './GomokuBoard'
 
 function GomokuGame() {
-    const [boardData, setBoardData] = useState(null);
+    const [boardData, setBoardData] = useState(null)
+    const [gameResult, setGameResult] = useState<string | null>(null)
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/gomoku/play')
+        fetchBoardData()
+    }, [])
+
+    const fetchBoardData = () => {
+        axios
+            .get('http://localhost:3000/api/gomoku/play')
             .then((response) => {
-                console.log(response.data.board.tiles)
-                setBoardData(response.data.board);
+                console.log('Received data after reset:', response.data) // Log the response data
+                setBoardData(response.data)
             })
             .catch((error) => {
-                console.error('Ett fel uppstod:', error);
-            });
-    }, []);
+                console.error('An error occurred:', error)
+            })
+    }
+
+    const makeMove = (row: number, col: number) => {
+        axios
+            .post('http://localhost:3000/api/gomoku/make_move', { row, col })
+            .then((response) => {
+                setBoardData(response.data)
+
+                if (response.data.winner !== 0) {
+                    setGameResult(
+                        response.data.winner === 1
+                            ? 'Black player wins!'
+                            : 'White player wins!'
+                    )
+                } else if (response.data.winner === -1) {
+                    setGameResult("It's a tie!")
+                }
+            })
+            .catch((error) => {
+                console.error('An error occurred while making a move:', error)
+            })
+    }
+
+    /*-----------------VARIANT 1 --------------------*/
+    const resetGame = () => {
+        setGameResult(null)
+        setBoardData(null) // Reset boardData to null when resetting the game
+        // fetchBoardData()
+    }
+
+    /*-----------------VARIANT 2 --------------------*/
+    // useEffect(() => {
+    //     if (boardData === null) {
+    //         fetchBoardData()
+    //     }
+    // }, [boardData])
+
+    // useEffect(() => {
+    //     if (boardData === null) {
+    //         resetGame()
+    //     }
+    // }, [boardData])
+
+    // useEffect(() => {
+    //     if (gameResult === null) {
+    //         fetchBoardData()
+    //     }
+    // }, [gameResult])
+
+    /*-----------------VARIANT 4 --------------------*/
+    //  useEffect(() => {
+    //     if (gameResult !== null) {
+    //         resetGame();
+    //     }
+    // }, [gameResult]);
+
+    // useEffect(() => {
+    //     if (boardData === null && gameResult === null) {
+    //         fetchBoardData();
+    //     }
+    // }, [boardData, gameResult]);
+
+    // const handleResetAndFetch = () => {
+    //     resetGame();
+    //     fetchBoardData();
+    // };
+
+    /*-----------------VARIANT 3 --------------------*/
+    const handleResetAndFetch = () => {
+        resetGame()
+        fetchBoardData()
+    }
+
+    useEffect(() => {
+        if (gameResult === null && boardData === null) {
+            fetchBoardData()
+        }
+    }, [gameResult, boardData])
 
     return (
         <div className="gomoku-game-area">
-            {boardData ? <GomokuBoard boardData={boardData} /> : <p className="loading">Wait</p>}
+            {boardData ? (
+                <div>
+                    {gameResult ? (
+                        <div className="game-result">
+                            <p>{gameResult}</p>
+                            <button
+                                className="prompt-btn"
+                                onClick={handleResetAndFetch}
+                            >
+                                OK
+                            </button>
+                        </div>
+                    ) : (
+                        <GomokuBoard
+                            boardData={boardData}
+                            makeMove={makeMove}
+                        />
+                    )}
+                </div>
+            ) : (
+                <p className="loading">Wait</p>
+            )}
         </div>
-    );
+    )
 }
 
-export default GomokuGame;
+export default GomokuGame
 
+// function GomokuGame() {
+//     const [boardData, setBoardData] = useState(null)
 
+//     /* -------------------- MIN VARIANT 1----------------- */
+//     useEffect(() => {
+//         fetchBoardData()
+//     }, [])
+
+//     const fetchBoardData = () => {
+//         axios
+//             .get('http://localhost:3000/api/gomoku/play')
+//             .then((response) => {
+//                 setBoardData(response.data)
+//             })
+//             .catch((error) => {
+//                 console.error('An error occurred:', error)
+//             })
+//     }
+
+//     const makeMove = (row, col) => {
+//         axios
+//             .post('http://localhost:3000/api/gomoku/make_move', { row, col })
+//             .then((response) => {
+//                 setBoardData(response.data)
+//             })
+//             .catch((error) => {
+//                 console.error('An error occurred while making a move:', error)
+//             })
+//     }
+
+//     return (
+//         <div className="gomoku-game-area">
+//             {boardData ? (
+//                 <GomokuBoard boardData={boardData} makeMove={makeMove} />
+//             ) : (
+//                 <p className="loading">Wait</p>
+//             )}
+//         </div>
+//     )
+// }
+
+// export default GomokuGame
+
+/* -----------------------CHRIS VARIANT------------------- */
+//     useEffect(() => {
+//         axios.get('http://localhost:3000/api/gomoku/play')
+//             .then((response) => {
+//                 console.log(response.data.board.tiles)
+//                 setBoardData(response.data.board);
+//             })
+//             .catch((error) => {
+//                 console.error('Ett fel uppstod:', error);
+//             });
+//     }, []);
+
+//     return (
+//         <div className="gomoku-game-area">
+//             {boardData ? <GomokuBoard boardData={boardData} /> : <p className="loading">Wait</p>}
+//         </div>
+//     );
+// }
+
+// export default GomokuGame;
+
+/* ------------------------ MIKALES VARIANT ------------------- */
 // import axios from 'axios'
 // import { useEffect } from 'react'
 // import GomokuBoard from './GomokuBoard'
