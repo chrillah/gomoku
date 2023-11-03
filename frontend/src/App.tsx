99da5e4c86a3f562cc7c62bbaa56bf8cfeae23ac
chrillah <ch.ronnberg@gmail.com>
2023-11-03 15:42:15 +0100

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
-import GomokuGame from "./components/GomokuGame"
-
-import PopUp from "./components/PopUp"
+import { useState } from 'react'
+import GomokuGame from './components/GomokuGame'
+import PopUp from './components/PopUp'
 
 function App() {
+    const [letsPlay, setLetsPlay] = useState(false)
+
+    function onPlay() {
+        setLetsPlay((prevState) => !prevState)
+    }
+
     return (
         <>
-            <h1 className='app-display-title'>Gomuko</h1>
-
-            {/* <PopUp /> */}
-
-            <GomokuGame />
+            {letsPlay ? (
+                <GomokuGame />
+            ) : (
+                <div className='start-a-new-game'>
+                    <h1 className="app-display-title">Gomuko</h1>
+                    <PopUp
+                        message={'Play'}
+                        buttonLabel={'yesbox'}
+                        onButtonClick={onPlay}
+                    />
+                </div>
+            )}
         </>
     )
 }
