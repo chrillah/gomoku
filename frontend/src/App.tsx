0c8f13725867ea6c5ce200eadf7946680f118673
chrillah <ch.ronnberg@gmail.com>
2023-11-09 15:42:41 +0100

    color set update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,27 @@ import PopUp from './components/PopUp'
 
 function App() {
     const [letsPlay, setLetsPlay] = useState(false)
+    const [color, setColor] = useState("1");
 
     function onPlay() {
         setLetsPlay((prevState) => !prevState)
     }
 
     return (
-        <>
+        <div className={`change-color-set-${color}`}>
             {letsPlay ? (
                 <GomokuGame onQuitGame={() => setLetsPlay(false)} />
             ) : (
-                <div className='start-a-new-game'>
-                    <h1 className="app-display-title">Gomuko</h1>
+                <div className="start-a-new-game">
+                    <div className="top-wrapper">
+                        <h1 className="app-display-title">Gomuko</h1>
+                        <div className="color-button-container">
+                            <button onClick={()=> setColor('1')} className="app-button">1</button>
+                            <button onClick={()=> setColor('2')}className="app-button">2</button>
+                            <button onClick={()=> setColor('3')}className="app-button">3</button>
+                            <button onClick={()=> setColor('4')}className="app-button">4</button>
+                        </div>
+                    </div>
                     <PopUp
                         message={'Play'}
                         buttonLabel={'yesbox'}
@@ -23,7 +32,7 @@ function App() {
                     />
                 </div>
             )}
-        </>
+        </div>
     )
 }
 
