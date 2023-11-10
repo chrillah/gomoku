412e31c03cc4ea52f82ea47e9afc3a35021c79aa
chrillah <ch.ronnberg@gmail.com>
2023-11-10 11:01:20 +0100

    fix

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
-
+// Insert your config here
 };
 
 const app = initializeApp(firebaseConfig);
