fbc9451a4e04a748270f3b2f63cd06bc1015b88a
Cuba Gustafsson <trill007@hotmail.com>
2023-11-27 11:10:12 +0100

    Gomoku Test new

--- backend/__tests__/isTie_test.js
+++ backend/__tests__/isTie_test.js
@@ -1,36 +1,38 @@
 // Funktion för att kontrollera om spelet är oavgjort
 function isTie(board) {
-    // Logik för att kontrollera om tavlan är full utan en vinnare för att förklara oavgjort
-    for (let row of board) {
-        for (let cell of row) {
-            if (cell === 0) {
-                return false // Det finns en tom cell, spelet är inte oavgjort
-            }
-        }
+  // Logik för att kontrollera om tavlan är full utan en vinnare för att förklara oavgjort
+  for (let row of board) {
+    for (let cell of row) {
+      if (cell === 0) {
+        return false; // Det finns en tom cell, spelet är inte oavgjort
+      }
     }
-    return true // Tavlan är full, och ingen vinnare utses, det är oavgjort
+  }
+  return true; // Tavlan är full, och ingen vinnare utses, det är oavgjort
 }
 
+//new test
+
 // Ett Jest-test för isTie funktionen ovan
-test('Check if isTie correctly identifies a tie on a full board', () => {
-    const fullBoardWithNoWinner = [
-      [1, 2, 1],
-      [2, 1, 2],
-      [2, 1, 1],
-    ];
+test("Check if isTie correctly identifies a tie on a full board", () => {
+  const fullBoardWithNoWinner = [
+    [1, 2, 1],
+    [2, 1, 2],
+    [2, 1, 1],
+  ];
 
-    const result = isTie(fullBoardWithNoWinner);
+  const result = isTie(fullBoardWithNoWinner);
 
-    expect(result).toBe(true); // Förväntas att isTie returnerar sant för en full bräda utan vinnare
-  });
+  expect(result).toBe(true); // Förväntas att isTie returnerar sant för en full bräda utan vinnare
+});
 
-  test('Check if isTie correctly identifies a non-tie scenario', () => {
-    const nonFullBoard = [
-      [1, 0, 2],
-      [2, 1, 2],
-      [0, 1, 1],
-    ];
-    const result = isTie(nonFullBoard);
+test("Check if isTie correctly identifies a non-tie scenario", () => {
+  const nonFullBoard = [
+    [1, 0, 2],
+    [2, 1, 2],
+    [0, 1, 1],
+  ];
+  const result = isTie(nonFullBoard);
 
-    expect(result).toBe(false); // Förväntas att isTie returnerar false för en bräda med tomma celler
-  });
+  expect(result).toBe(false); // Förväntas att isTie returnerar false för en bräda med tomma celler
+});
